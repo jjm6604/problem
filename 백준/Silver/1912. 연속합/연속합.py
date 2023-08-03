1d0f21c58cebeee146240ca422bf0b7548b3ee92
지민 <81950388+jjm6604@users.noreply.github.com>
2023-08-03 21:10:13 +0900

    Update 연속합.py

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -4,9 +4,6 @@ A = list(map(int, input().split()))
 B = [0] * N
 B[0] = A[0]
 for i in range(1, len(A)):
-    if B[i-1] + A[i] < A[i]:
-        B[i] = A[i]
-    else:
-        B[i] = B[i-1] + A[i]
+    B[i] = max(B[i-1] + A[i], A[i])
 
 print(max(B))
