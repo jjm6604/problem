31b756bb2b9fcef34bf1dfe893fce8db2e672647
지민 <81950388+jjm6604@users.noreply.github.com>
2023-09-23 15:15:26 +0900

    [Silver V] Title: 막대기, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -1,11 +1,3 @@
-n = int(input())
-k = 64
-cnt = 0
-
-while n > 0:
-    if n >= k:
-        cnt += 1
-        n -= k
-    else:
-        k //= 2
-print(cnt)
+N = int(input())
+res = bin(N).count('1')
+print(res)
