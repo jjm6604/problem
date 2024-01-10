f57fe4dd74119feee4bae3e5009c3651be7af3f7
지민 <81950388+jjm6604@users.noreply.github.com>
2024-01-10 21:34:16 +0900

    [Silver I] Title: 영역 구하기, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -15,21 +15,17 @@ for i in range(N):
         if MAP[i][j] == 0 and v[i][j] == 0:
             cnt = 1
             res += 1
-            x, y = i, j
-            s = []
+            q = [(i, j)]
             v[i][j] = 1
-            while True:
+            while q:
+                x, y = q.pop(0)
                 for k in range(4):
                     dx = x + direct[k][0]
                     dy = y + direct[k][1]
                     if 0 <= dx < N and 0 <= dy < M and v[dx][dy] == 0 and MAP[dx][dy] == 0:
                         v[dx][dy] = 1
-                        s.append((dx, dy))
+                        q.append((dx, dy))
                         cnt += 1
-                if s:
-                    x, y = s.pop()
-                else:
-                    break
             lst.append(cnt)
 lst.sort()
 print(res)
