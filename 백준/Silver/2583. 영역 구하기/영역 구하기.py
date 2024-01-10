c65002f952dcd7b11c275e1bdd29a8770160d985
지민 <81950388+jjm6604@users.noreply.github.com>
2024-01-10 21:27:45 +0900

    [Silver I] Title: 영역 구하기, Time: 132 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,36 +1,32 @@
-di = [1, 0, -1, 0]
-dj = [0, 1, 0, -1]
-result = []
-
+direct = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 M, N, K = map(int, input().split())
-lst = [[0] * M for _ in range(N)]
+
+MAP = [[0] * M for _ in range(N)]
+v = [[0] * M for _ in range(N)]
+lst = []
+res = 0
 for k in range(K):
     x1, y1, x2, y2 = map(int, input().split())
     for i in range(x1, x2):
         for j in range(y1, y2):
-            lst[i][j] = 1
-v = [[0] * M for _ in range(N)]
-coord = []
+            MAP[i][j] = 1
 for i in range(N):
     for j in range(M):
-        if lst[i][j] == 0 and v[i][j] == 0:
+        if MAP[i][j] == 0 and v[i][j] == 0:
             cnt = 1
-            x, y = i, j
+            res += 1
+            q = [(i, j)]
             v[i][j] = 1
-            while True:
+            while q:
+                x, y = q.pop(0)
                 for k in range(4):
-                    dx = x + di[k]
-                    dy = y + dj[k]
-                    if 0 <= dx < N and 0 <= dy < M and lst[dx][dy] == 0 and v[dx][dy] == 0:
-                        cnt += 1
+                    dx = x + direct[k][0]
+                    dy = y + direct[k][1]
+                    if 0 <= dx < N and 0 <= dy < M and v[dx][dy] == 0 and MAP[dx][dy] == 0:
                         v[dx][dy] = 1
-                        coord.append([dx, dy])
-                else:
-                    if coord:
-                        x, y = coord.pop()
-                    else:
-                        break
-            result.append(cnt)
-result.sort()
-print(len(result))
-print(*result)
+                        q.append((dx, dy))
+                        cnt += 1
+            lst.append(cnt)
+lst.sort()
+print(res)
+print(*lst)
